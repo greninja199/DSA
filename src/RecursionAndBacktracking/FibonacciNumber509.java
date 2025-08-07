9edc3ffa21c1c899e700188b565d850657e4937e
pradumank199 <pradumank199@gmail.com>
2025-08-07 13:44:54 +0530

    24. Swap Nodes in Pairs

--- src/RecursionAndBacktracking/FibonacciNumber509.java
+++ src/RecursionAndBacktracking/FibonacciNumber509.java
@@ -3,6 +3,19 @@ package RecursionAndBacktracking;
 import java.util.HashMap;
 
 public class FibonacciNumber509 {
+    public int fib(int n) {
+        if(n==0)
+            return 0;
+        int a=0;
+        int b=1;
+        for(int i=2;i<n;i++){
+            b=a+b;
+            a=b-a;
+        }
+        return a+b;
+    }
+}
+/*
     HashMap<Integer, Integer> fibMapper = new HashMap<>();
     public int fib(int n) {
         if(fibMapper.containsKey(n))
@@ -18,4 +31,5 @@ public class FibonacciNumber509 {
         fibMapper.put(n,fib(n-1)+fib(n-2));
         return fibMapper.get(n);
     }
-}
+
+     */
---
+++ src/RecursionAndBacktracking/SwapNodesInPairs24.java
@@ -0,0 +1,15 @@
+package RecursionAndBacktracking;
+
+import LinkedList9.ListNode;
+
+public class SwapNodesInPairs24 {
+    public ListNode swapPairs(ListNode head) {
+        if(head==null || head.next==null)
+            return head;
+        ListNode nextHead = head.next.next;
+        head.next.next = head;
+        head = head.next;
+        head.next.next = swapPairs(nextHead);
+        return head;
+    }
+}
